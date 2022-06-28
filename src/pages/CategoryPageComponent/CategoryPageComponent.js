89c571177abcaad5accb499d9fa7838f479526c3
Janani De Silva <desilvapkj.19@itfac.mrt.ac.lk>
2022-06-29 03:15:01 +0530

    Added service category#10

--- src/pages/CategoryPageComponent/CategoryPageComponent.js
+++ src/pages/CategoryPageComponent/CategoryPageComponent.js
@@ -1,2 +1,29 @@
 /*eslint-disable */
 //category page component
+/*eslint-disable */
+import React, { useState} from 'react';
+import './CategoryPageComponent.scss';
+import { addcatagory} from '../../services/AuthService';
+import CatagoryList from './CategoryListComponent/CategoryListComponent.js';
+import AdminNavComponent from '../../components/AdminNavComponent/AdminNavComponent';
+import SearchBarComponent from '../../components/SearchBarComponent/SearchBarComponent';
+
+const CategoryPageComponent=()=> {
+
+  
+    const handleCatagorySubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await addcatagory({
+                name: e.target.catagory.value,
+                addedOn: currentDate,
+                updatedOn: currentDate,
+            });
+            setReload(!reload);
+            console.log(response);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+}
+export default CategoryPageComponent;
