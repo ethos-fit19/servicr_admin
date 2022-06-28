829db7e065cb0d0da48c99d45575a95c011b3d17
Janani De Silva <desilvapkj.19@itfac.mrt.ac.lk>
2022-06-29 02:53:04 +0530

    Added service category#5

--- src/pages/CategoryPageComponent/EditCategoryComponent/EditCategoryComponent.js
+++ src/pages/CategoryPageComponent/EditCategoryComponent/EditCategoryComponent.js
@@ -29,13 +29,59 @@ const EditCategoryComponent = ({ trigger, settrigger, catagory_id }) => {
 
 
 
-  
+    const loadCategory = async () => {
+
+        try {
+            const response = await loadCat(catagory_id);
+            console.log(response.data.data);
+            setCategory(response.data.data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+
+    const handleCatagoryEditSubmit = async (e) => {
+        try {
+            const response = await editcategory({
+                id: catagory_id,
+                name: e.target.catagory.value,
+                updatedOn: currentDate,
+            });
+            console.log(response);
+            settrigger(false);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    return trigger ? (
+        <div className='create_folder_popup'>
+            <div className='create_folder_popup-body'>
+                <button className='closebtn'
+                    onClick={() => settrigger(false)}>
+                    <FiX />
+                </button>
+                <div className='create_folder' ref={ref}>
+                    <div className='create_folder_box'>
+                        <form id='form' className='addcatagory' onSubmit={handleCatagoryEditSubmit}>
+                            <input type='text'
+                                placeholder='add category'
+                                className='addcatagory'
+                                name='catagory'
+                                defaultValue={category && category.name}
+                                required
+                            ></input>
+                            <button type='submit' className='btnaddcatagory'>Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) : <></>;
 };
 
 export default EditCategoryComponent;
-
-
-
 
 
 
